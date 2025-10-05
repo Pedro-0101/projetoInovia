ce14b1f474ac0a2b32f802fb0e16c6a185a753d8
pedro-0101m <pedro.tiete@outlook.com>
2025-10-05 20:27:58 -0300

    criado calendario

---
+++ web/inovia/src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Callendar from "./components/Calendar";
+
+function App() {
+  return <div className="App">
+    <Callendar />
+  </div>;
+}
+
+export default App;
